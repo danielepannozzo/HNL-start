082c48c14dfe30fd28c24807aeeeffda33bf427f
danielepannozzo <79516214+danielepannozzo@users.noreply.github.com>
2021-03-10 14:13:46 +0100

    Update Diff_e.cc
    Totally modified the file -- the previous version was shit

--- Diff_e.cc
+++ Diff_e.cc
@@ -16,33 +16,41 @@ int main() {
     HNLClass signal;
     signal.Init(cosa);
 
-    int i=0,j=0,l=0;
+    int i=0,j=0,t=0;
+    int eprobe=0, egen=0;
 
-    TH1F *Ne = new TH1F("Ne", "Difference bet/ e gen and reco", 250, -0.3, 5.);
-    TCanvas *c1 = new TCanvas("#E", "Electron number", 1280, 1024);
+    TH1F *Ne = new TH1F("Ne", "Difference bet/ e gen and reco", 50, -3.3, 4.3);
+    //TH1F *Ne2 = new TH1F("Ne2", "", 50, -0.3, 50.);
+    TCanvas *c1 = new TCanvas("#E", "Diff electron number", 1280, 1024);
 
 
     for(i=0;i<signal.fChain->GetEntries();i++) {
-        
+
+        TLorentzVector tmp1,tmp2;
         signal.fChain->GetEntry(i);
-        l=0;
-        int n=0;
+
+        egen=0;
+        eprobe=0;
+
         for(j=0;j<signal.nGenPart;j++){
             if(abs(signal.GenPart_pdgId[j])==11){
-                
-                l+=1;
-            }
-            n=l-signal.nElectron;
+                egen++;
+                for(t=0;t<signal.nElectron;t++){  
+                    if(signal.Electron_genPartIdx[t]==j) eprobe++;
+                };
+            };
         };
+        int n=egen-eprobe;
         Ne->Fill(n);
     };
-
     c1->cd(1);
-    Ne->GetXaxis()->SetTitle("Difference");
+    Ne->GetXaxis()->SetTitle("n_{ele^{gen}} - n_{ele^{reco}}");
     Ne->GetYaxis()->SetTitle("Number of entries");
-    Ne->Draw("HIST");
+    Ne->SetFillColor(kBlue);
+    //Ne->Scale(1./Ne->GetEntries());
+    Ne->DrawNormalized("HIST");
 
-    c1->SaveAs("Diff_e.png");
+    c1->SaveAs("Diff_e.jpg");
 
     delete cosa;
 
